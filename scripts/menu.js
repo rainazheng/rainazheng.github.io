113ed0022c2dedfaa2a3f6408b7b9a112644b0f5
Raina <raina.zheng@gmail.com>
2019-01-18 17:53:29 -0600

    fix menu, add fav icon

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,9 @@
 {
   $('#nav-icon').click(function() {
-    console.log("Toggling menu");
+    $('#nav-icon, #menu').toggleClass('open');
+  });
+
+  $('#menu a').click(function() {
     $('#nav-icon, #menu').toggleClass('open');
   });
 }
-
-// function openMenu(x) {
-//   x.classList.toggle('open');
-//
-// }
