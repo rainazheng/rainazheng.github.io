cc1bb534f708529f7eb596630a10dbd2acb58cd1
Raina <raina.zheng@gmail.com>
2019-01-18 18:40:48 -0600

    added working menu links, smooth scrolling, and working steppers

---
+++ scripts/localWaypoints.js
@@ -0,0 +1,43 @@
+var projectWaypoint = new Waypoint({
+  element: document.getElementById('below-hero'),
+  handler: function(direction) {
+    if(direction="down"){
+      console.log("Scrolling down to project");
+      $("#stepTwo").toggleClass("activeStepper");
+      $("#stepOne").toggleClass("activeStepper");
+    } else{
+      console.log("Scrolling up to project");
+      $("#stepTwo").toggleClass("activeStepper");
+      $("#stepThree").toggleClass("activeStepper");
+    }
+  },
+  offset: 50
+});
+
+var aboutWaypoint = new Waypoint({
+  element: document.getElementById('below-projects'),
+  handler: function(direction) {
+    if(direction="down"){
+      console.log("Scrolling down to about ");
+      $("#stepThree").toggleClass("activeStepper");
+      $("#stepTwo").toggleClass("activeStepper");
+    } else{
+      console.log("Scrolling up to about");
+      $("#stepThree").toggleClass("activeStepper");
+      $("#stepFour").toggleClass("activeStepper");
+    }
+  },
+  offset: 50
+});
+
+var contactWaypoint = new Waypoint({
+  element: document.getElementById('below-about'),
+  handler: function(direction) {
+    if(direction="down"){
+      console.log("Scrolling down to contact");
+      $("#stepFour").toggleClass("activeStepper");
+      $("#stepThree").toggleClass("activeStepper");
+    }
+  },
+  offset: 50
+})
